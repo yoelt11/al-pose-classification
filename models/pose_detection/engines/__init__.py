0f983f225b88b0ae9ef4335e4de85dbea6215d13
Mac Developer <torres@technology.de>
2023-05-26 13:06:53 +0200

    adapted to work with mac

--- models/pose_detection/engines/__init__.py
+++ models/pose_detection/engines/__init__.py
@@ -1,3 +1,4 @@
-from .posenet import PoseNet
+# -- uncomment desired engine
+#from .posenet import PoseNet
 from .yolov7_pose import YoloV7
-from .movenet_coral import Movenet
+#from .movenet_coral import Movenet
